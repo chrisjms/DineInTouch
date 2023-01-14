225d2ff74f85e1732cfbf2e6ecd5f1c5a53f8843
Christopher James <chriisjms@gmail.com>
2023-01-14 11:44:02 +0100

    initial commit

---
+++ DineInTouch/DineInTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DineInTouchApp.swift
+//  DineInTouch
+//
+//  Created by Chris on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct DineInTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
